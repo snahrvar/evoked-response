0f13ef19d9428c8e651df0896d097b08da342fa5
Matt Eason <matt@oasiscode.com>
2021-05-10 09:27:39 -0400

    Added scroll animations. Removed white bar above ultrahuman testimonial

--- js/transitions.js
+++ js/transitions.js
@@ -24,3 +24,46 @@ $(".adam-button").click(function () {
     1000
   );
 });
+
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend
+    );
+};
+
+const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+        elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+
+const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+        if (elementInView(el, 1.25)) {
+            displayScrollElement(el);
+        }
+    })
+}
+
+window.addEventListener("scroll", () => {
+    handleScrollAnimation();
+});
+
+window.addEventListener("load", () => {
+    handleScrollAnimation();
+});
